7174b2247e0f9512135d4381ea7fb472182124d9
omgitsbillryan <bill.ryan@adhocteam.us>
2018-12-20 12:45:08 -0500

    test

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,9 +1,8 @@
 #!/usr/bin/env groovy
-import org.kohsuke.github.GitHub
 
-def call(String msg, Map args) {
-  boolean notifySlack = args.notifySlack // 'null' defaults to false
-  String color = (args.color == null) ? 'good' : args.color
+def call(String msg) {
+  // boolean notifySlack = args.notifySlack // 'null' defaults to false
+  // String color = (args.color == null) ? 'good' : args.color
 
-  echo "inside notify, notifySlack=${notifySlack}... color=${color}"
+  echo "${msg}, notifySlack=${notifySlack}... color=${color}"
 }
