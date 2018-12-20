a613d6bdda42b8e0b769520fc97f330d5a3218aa
omgitsbillryan <bill.ryan@adhocteam.us>
2018-12-20 12:46:49 -0500

    stuff

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String msg) {
+def call(String msg, Map args) {
   // boolean notifySlack = args.notifySlack // 'null' defaults to false
   // String color = (args.color == null) ? 'good' : args.color
 
-  echo "${msg}, notifySlack=${notifySlack}... color=${color}"
+  echo "${msg}, args=${args}"
 }
