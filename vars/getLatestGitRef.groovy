4252de3e26e0ac06017797309cad4fcb7fd68a07
omgitsbillryan <bill.ryan@adhocteam.us>
2018-12-20 19:01:53 -0500

    additional null check

--- vars/getLatestGitRef.groovy
+++ vars/getLatestGitRef.groovy
@@ -4,7 +4,7 @@ import bill.ryan.GlobalVars
 
 def call(Map args=null, String repo) {
   echo "Vars: args=[${args}] repo=[${repo}] GH_ORG=[${GlobalVars.GH_ORG}]"
-  String branch = (args.branch == null) ? 'master' : args.branch
+  String branch = (args == null || args.branch == null) ? 'master' : args.branch
 
   def github = GitHub.connectAnonymously()
   def r = github.getRepository(GlobalVars.GH_ORG + '/' + repo)
